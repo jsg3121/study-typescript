732d5319188822a2b61ea077bada6a1654aedf5b
jsg3121 <xodm95@gmail.com>
2020-11-16 23:37:08 +0900

    타입스크립트 컴파일하기

---
+++ personES3.js
@@ -0,0 +1,13 @@
+// var Person = /** @class */ (function () {
+//     function Person(name) {
+//         this.name = name;
+//     }
+//     Person.prototype.sayHello = function () {
+//         return "Hello, " + this.name;
+//     };
+//     return Person;
+// }());
+// var person = new Person("lee");
+// console.log(person.sayHello());
+
+// person.ts 에서 ES3 버전으로 컴파일
---
+++ personES6.js
@@ -0,0 +1,12 @@
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     sayHello() {
+//         return "Hello, " + this.name;
+//     }
+// }
+// const person = new Person("lee");
+// console.log(person.sayHello());
+
+// person.ts 에서 ES6 버전으로 컴파일
